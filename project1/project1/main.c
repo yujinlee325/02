e8e950d6f78c1eed3ef6fffb1c086ef65c87ab46
yujinlee325 <yu_jin325@naver.com>
2020-09-10 16:20:38 +0900

    +

--- project1/project1/main.c
+++ project1/project1/main.c
@@ -9,6 +9,12 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello, Sookmyung Electronics!\n");
+    
+    int a, b;
+    a = 10;
+    b = 20;
+    
+    printf("%d + %d = %d\n", a, b, a+b);
+    
     return 0;
 }

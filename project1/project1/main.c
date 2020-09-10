d204eb1b9109ed082deed8fb8c6f0f3028945494
yujinlee325 <yu_jin325@naver.com>
2020-09-10 16:07:39 +0900

    0910

---
+++ project1/project1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  project1
+//
+//  Created by 이유진 on 2020/09/10.
+//  Copyright © 2020 이유진. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello, Sookmyung Electronics!\n");
+    return 0;
+}
